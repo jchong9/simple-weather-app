c852099c4c1e5b7ab0987bc59fe7569e012ab1e3
Jeremy Chong <chong.jeremy@hotmail.com>
2024-05-01 13:58:23 -0400

    Added API call and a message to display the current weather

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const request = require('request');
 
 let api_key = 'eb593744434a8f32cc3f870e1d720b3a';
 let city = 'portland';
-let url = 'http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}'
+let url =  `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api_key}&units=metric`;
 
 request(url, function(err, response, body) {
     if (err) {
         console.log("err: ", err);
     }
     else {
-        console.log("body: ", body);
+        let weather = JSON.parse(body);
+        let msg = `It's ${weather.list[0].main.temp} degrees in ${weather.city.name}`;
+        console.log(msg);
     }
 });
