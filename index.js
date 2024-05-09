baa414a8f0f1d86644fc6fad21d0899d1ab2c737
Jeremy Chong <chong.jeremy@hotmail.com>
2024-05-09 18:00:22 -0400

    added and linked static css file to html

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const request = require('request');
+const path = require('path');
+const express = require('express');
+const app = express();
 
-let api_key = 'eb593744434a8f32cc3f870e1d720b3a';
-let city = 'portland';
-let url =  `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api_key}&units=metric`;
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
 
-request(url, function(err, response, body) {
-    if (err) {
-        console.log("err: ", err);
-    }
-    else {
-        let weather = JSON.parse(body);
-        let msg = `It's ${weather.list[0].main.temp} degrees in ${weather.city.name}`;
-        console.log(msg);
-    }
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
 });
